558358e09731ec9ba8deb4d8cd7845492999b7d6
Ntolgka <nalbanttolga1@gmail.com>
2023-12-22 23:34:29 +0300

    Database configuration done and Coupon Read methods created

--- Mango.Services.CouponAPI/Models/Dto/CouponDto.cs
+++ Mango.Services.CouponAPI/Models/Dto/CouponDto.cs
@@ -1,14 +1,9 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Mango.Services.CouponAPI.Models
+﻿namespace Mango.Services.CouponAPI.Models.Dto
 {
-    public class Coupon
+    public class CouponDto
     {
-        [Key]
         public int CouponId { get; set; }
-        [Required]
         public string CouponCode { get; set; }
-        [Required]
         public double DiscountAmount { get; set; }
         public int MinAmount { get; set; }
     }
